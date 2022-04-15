62e207e09cc17824062eb898764d211ab4fac7d3
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-04-15 11:04:30 +0530

    Add files via upload

---
+++ Number_system_part2.py
@@ -0,0 +1,47 @@
+from fractions import Fraction as F
+# print(F(6,2))#3
+# print(F(2.5))#5/2
+# print(F('2.5'))#5/2
+# print(F("2.5"))#5/2
+# print(F(4.5))#9/2
+# print(F(5,0))#ZeroDivisionError
+# b=5/0
+# print(F(b))#ZeroDivisionError
+# v=F(5,20)
+# print(v)#1/4
+
+# a=1+2j
+# print(a,type(a))#1+2j complex
+# print(a.real)#1.0
+# print(a.imag)#2.0
+# h=4*4.3j
+# print(h,type(h))#17.2j complex
+# print(h.real)#0.0
+# print(h.imag)#17.2
+
+# print(1e0)#1.0
+# print(4e0)#4.0
+# print(1e1)#10.0
+# print(1e2)#100.0
+# print(12e2)#1200.0
+# print(1.4e0)#1.4
+# print(1.4e1)#14.0
+# print(1.4e2)#140.0
+# print(1.4e3)#1400.0
+
+# print(4*5)#20
+# print(2**3)#8
+# print(4**3)#64
+# print(2**4)#16
+# print(1.5*2)#2.25
+
+# import math as m
+# print(m.exp(3))#2.71828*2.71828*2.71828=20.08553
+# print(m.exp(0))#1.0
+# print(m.exp(1))#2.71828
+
+
+
+
+
+
